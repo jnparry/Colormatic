02d3b97f97eb61818996dc70beb76838dad532e5
Jordan <jordanwahparry@gmail.com>
2017-10-30 11:09:31 -0600

    Setting up the camera manager

--- app/src/main/java/com/example/jordan/colormatic/MainActivity.java
+++ app/src/main/java/com/example/jordan/colormatic/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.jordan.colormatic;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics characteristics
+                        = manager.getCameraCharacteristics(cameraId);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
