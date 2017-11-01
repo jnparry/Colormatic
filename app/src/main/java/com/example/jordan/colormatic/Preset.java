d814b3438d7cee2a69a1bcdfe2e7602566c34686
loganes1 <sau13007@byui.edu>
2017-11-01 14:48:39 -0600

    Added the Preset class as a stub.

---
+++ app/src/main/java/com/example/jordan/colormatic/Preset.java
@@ -0,0 +1,12 @@
+package com.example.jordan.colormatic;
+
+/**
+ * Created by Student Profile on 11/1/2017.
+ */
+
+public class Preset {
+    String presetName;
+
+    String getString() {return presetName;}
+    void setString(String name) {presetName = name;}
+}
