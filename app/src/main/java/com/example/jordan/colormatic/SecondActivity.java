a2fd40431a197ae64b169568a98903e2fdea2461
cdvankammen <31197020+cdvankammen@users.noreply.github.com>
2017-11-09 01:10:08 -0700

    WEEK 9 wednesday, Updated the Second Activity allows data to be saved and pushed to dispay on the screen.

--- app/src/main/java/com/example/jordan/colormatic/SecondActivity.java
+++ app/src/main/java/com/example/jordan/colormatic/SecondActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,25 +19,70 @@ public class SecondActivity extends AppCompatActivity {
 
     public static final String APP_PREFS = "APPLICATION_PREFERENCES";
     String _text;
+    //private object used to reference userText box : type EditText
+    private EditText userText;
+    // object used to save string from userText box
+    private String userName;
+    private String text;
+
+
+    private TextView displayText;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Intent intent = getIntent();
+
         _text = intent.getStringExtra(MainActivity.TEST_TEXT);
+
         TextView label = (TextView) findViewById(R.id.displayText);
         label.setText(_text);
+
+        //userText boxed is referenced and initialized for use in program
+        userText = (EditText) findViewById(R.id.userText);
+
+        //sets a value for user text default
+        text = "input your preset name";
+        userText.setText(text);
+
+        //Creates the save button and values
+        Button saveBttn = (Button) findViewById(R.id.saveBttn);
+        assert saveBttn != null;
+
+        //The display text object and initialization
+        displayText = (TextView) findViewById(R.id.displayText);
+        message = "YOUR Text from above loads here when read in";
+        displayText.setText(message);
+
+        // Finds the object and what the button will do when pressed
+        saveBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText();
+            }
+        });
     }
 
-//    protected void saveText() {
-//        SharedPreferences sharedPrefs = getSharedPreferences(MainActivity.APP_PREFS, Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPrefs.edit();
-//
-//        editor.putString(MainActivity.TEST_TEXT, _text);
-//
-//        Toast.makeText(this, "Saved Text", Toast.LENGTH_SHORT).show();
-//    }
+    protected void saveText() {
+
+        SharedPreferences sharedPrefs = getSharedPreferences(MainActivity.APP_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        editor.putString(MainActivity.TEST_TEXT, _text);
+        Toast.makeText(this, "Saved Text", Toast.LENGTH_SHORT).show();
+
+        userName = userText.getText().toString();
+        editor.putString("USER_NAME", userName);
+        Toast.makeText(this, "USER's NAME: Saved", Toast.LENGTH_SHORT).show();
+
+        editor.apply();
+        message = "Updated text in preset name to:"  + userName;
+        displayText.setText(message);
+
+
+   }
 
 //    protected void loadText() {
 //        SharedPreferences sharedPref = getSharedPreferences(APP_PREFS, Context.MODE_PRIVATE);
