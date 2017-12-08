a5886822309b8428c1dd97ea6ba2b65217ffdb74
cdvankammen <31197020+cdvankammen@users.noreply.github.com>
2017-12-08 13:37:14 -0700

    WEEK 12 IT MAKES THE FILE

--- app/src/main/java/com/example/jordan/colormatic/MainActivity.java
+++ app/src/main/java/com/example/jordan/colormatic/MainActivity.java
@@ -194,7 +194,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String ColormaticImageFilePathDirectory = createDirectoryAndSaveFile();
+
+        ColormaticImageFilePathDirectory = createDirectoryAndSaveFile();
 
 
         textureView = findViewById(R.id.texture);
